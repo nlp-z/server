b4e1da79f78b6b94b738cdda6ab366563541860e
HOANG THE ANH <therealhoangtheanh@gmail.com>
2024-01-24 20:34:05 +0700

    Initialization

---
+++ src/main/java/com/apcs/nlpz/NlpzApplication.java
@@ -0,0 +1,13 @@
+package com.apcs.nlpz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlpzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlpzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apcs/nlpz/NlpzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apcs.nlpz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlpzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
